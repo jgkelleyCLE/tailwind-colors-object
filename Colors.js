9c9dfbda86e9310fe8f3fdb54f73b339c978c392
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2023-11-08 09:32:47 -0500

    Added zinc

--- Colors.js
+++ Colors.js
@@ -29,6 +29,19 @@ export const COLORS = {
     slate900: "#0f172a",
     slate950: "#020617",
 
+    //ZINC
+    zinc50: "#fafafa",
+    zinc100: "#f4f4f5",
+    zinc200: "#e4e4e7",
+    zinc300: "#d4d4d8",
+    zinc400: "#a1a1aa",
+    zinc500: "#71717a",
+    zinc600: "#52525b",
+    zinc700: "#3f3f46",
+    zinc800: "#27272a",
+    zinc900: "#18181b",
+    zinc950: "#09090b",
+
     //STONE
     stone50: "#fafaf9",
     stone100: "#f5f5f4",
